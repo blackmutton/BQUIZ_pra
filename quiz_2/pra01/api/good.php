ae9d3b99cdfdbc876d63d7e86b0a24ac32184f3c
yt Chen <yutongc30@gmail.com>
2024-08-06 23:14:06 +0800

    增加對文章按讚數更新的功能

--- quiz_2/pra01/api/good.php
+++ quiz_2/pra01/api/good.php
@@ -7,9 +7,23 @@ include "base.php";
     // [news] => 1
 // )
 $chk=$Log->count($_POST);
-
+$news=$News->find($_POST['news']);
+// dd($news);
+// Array
+// (
+    // [id] => 1
+    // [title] => 缺乏運動已成為影響全球死亡率的第四大危險因子
+    // [article] =>""
+    // [type] => 1
+    // [sh] => 1
+    // [good] => 10
+// }
 if($chk>0){
     $Log->del($_POST);
+    $news['good']--;
 }else{
     $Log->save($_POST);
+    $news['good']++;
 }
+
+$News->save($news);
