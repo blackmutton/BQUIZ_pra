6c9dc53af0b23f8a68dab25288da6e811b2ca8dd
yt Chen <yutongc30@gmail.com>
2024-08-07 15:08:35 +0800

    建立我要投票頁面

---
+++ quiz_2/pra01/front/vote.php
@@ -0,0 +1,25 @@
+<!-- 從que.php複製來的，table那塊都不要 -->
+<?php
+$subject = $Que->find($_GET['id']);
+?>
+<fieldset>
+    <legend>目前位置:首頁 > 問卷調查 > <?= $subject['text'] ?></legend>
+    <h4><?= $subject['text'] ?></h4>
+    <?php
+    $options = $Que->all(['subject_id' => $subject['id']]);
+    foreach ($options as $option) {
+        echo "<p>";
+        echo "<input type='radio' name='opt' value='{$option['id']}'>";
+        echo $option['text'];
+        echo "</p>";
+    }
+    ?>
+    <div class="ct">
+        <button onclick='vote()'>我要投票</button>
+    </div>
+</fieldset>
+<script>
+    function vote() {
+
+    }
+</script>
