7702e53ee96983d373280c93addcca920aefae95
yt Chen <yutongc30@gmail.com>
2024-08-08 18:32:31 +0800

    建立會員註冊畫面

---
+++ quiz_2/pra02/front/reg.php
@@ -0,0 +1,34 @@
+<fieldset>
+    <legend>會員註冊</legend>
+    <div>*請設定您要註冊的帳號及密碼</div>
+    <table>
+        <tr>
+            <td class="clo">Step1:登入帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step2:登入密碼</td>
+            <td><input type="text" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step3:再次確認密碼</td>
+            <td><input type="text" name="pw2" id="pw2"></td>
+        </tr>
+        <tr>
+            <td class="clo">Step4:信箱(忘記密碼時使用)</td>
+            <td><input type="text" name="email" id="email"></td>
+        </tr>
+        <tr>
+            <td class="clo">
+                <button onclick="reg()">註冊</button>
+                <button onclick="clean()">清除</button>
+            </td>
+            <td></td>
+        </tr>
+    </table>
+</fieldset>
+<script>
+    function reg(){
+        
+    }
+</script>
