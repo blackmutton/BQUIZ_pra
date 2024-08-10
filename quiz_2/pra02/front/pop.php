b9113679a6db4f70637372681eeb57d217f08170
yt Chen <yutongc30@gmail.com>
2024-08-10 20:32:02 +0800

    完成最新文章和人氣文章的顯示及分頁功能

---
+++ quiz_2/pra02/front/pop.php
@@ -0,0 +1,45 @@
+<!-- 從news.php複製而來 -->
+<div>
+    目前位置: 首頁 > 人氣文章區
+</div>
+
+<table class="tab">
+    <tr>
+        <td width="30%">標題</td>
+        <td width="50%">內容</td>
+        <td>人氣</td>
+    </tr>
+    <?php
+    $total=$News->count(['sh'=>1]);
+    $div=5;
+    $pages=ceil($total/$div);
+    $now=$_GET['p']??1;
+    $start=($now-1)*$div;
+    $rows=$News->all(['sh'=>1]," order by `good` desc limit $start,$div");
+    foreach($rows as $row){
+    ?>
+    <tr>
+        <td><?=$row['title']?></td>
+        <td><?=mb_substr($row['article'],0,30)?></td>
+        <td><?=$row['good']?></td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<div>
+<?php
+if($now-1>0){
+    $prev=$now-1;
+    echo "<a href='?do=news&p=$prev'> < </a>";
+}
+for($i=1;$i<=$pages;$i++){
+    $font=($_GET['p']==$i)??1;
+    echo "<a href='?do=news&p=$i'style='font-size:$font'>$i</a>";
+}
+if($now+1<=$pages){
+    $next=$now+1;
+    echo "<a href='?do=news&p=$next'> > </a>";
+}
+?>
+</div>
