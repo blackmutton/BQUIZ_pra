94bbc3f32f0695f7031051e4bc0fefa5d72c76d8
yt Chen <yutongc30@gmail.com>
2024-08-15 20:06:38 +0800

    把簡單的項目和版型先做完

--- quiz_2/pra02_real/index.php
+++ quiz_2/pra02_real/index.php
@@ -3,9 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>健康促進網</title>
-<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-<script src="./home_files/jquery-1.9.1.min.js"></script>
-<script src="./home_files/js.js"></script>
+<link href="./css/css.css" rel="stylesheet" type="text/css">
+<script src="./js/jquery-3.4.1.min.js"></script>
+<script src="./js/js.js"></script>
 </head>
 
 <body>
@@ -15,9 +15,10 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        <?=date("m月d日l")?> | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+		<a href="index.php"style="float:right">回首頁</a>
         <div id="title2">
-        	
+        	<a href="index.php"><img src="./icon/02B01.jpg" alt=""></a>
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
@@ -30,6 +31,9 @@
             <div class="hal" id="main">
             	<div>
             		
+                	<span style="width:78%; display:inline-block;">
+                    	                    	<marquee>請民眾踴躍投稿電子稿，讓電子報成為大家相互交流、分享的園地!詳情請見最新文章</marquee>
+                    	                    </span>
                 	<span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
@@ -41,7 +45,7 @@
         <div id="bottom">
     	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
     		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 
