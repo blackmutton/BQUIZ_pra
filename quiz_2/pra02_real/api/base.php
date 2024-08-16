d09b3723416e5937237ff8ac846f896b850821f9
yt Chen <yutongc30@gmail.com>
2024-08-16 23:15:57 +0800

    完成後台新聞區頁面

--- quiz_2/pra02_real/api/base.php
+++ quiz_2/pra02_real/api/base.php
@@ -102,6 +102,7 @@ function dd($array){
 
 $User=new DB("users");
 $Total=new DB("total");
+$News=new DB("news");
 
 if(!isset($_SESSION['total'])){
     if($Total->count(['date'=>date("Y-m-d")])>0){
