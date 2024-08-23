8ccff3552553d90c4ec766202212324ed2b155a4
yt Chen <yutongc30@gmail.com>
2024-08-23 22:12:31 +0800

    完成更多消息顯示及分頁功能

--- quiz_1/pra01/front/main.php
+++ quiz_1/pra01/front/main.php
@@ -74,6 +74,13 @@
 								$("#altt").hide()
 							}
 						)
+						$("#altt").hover{
+							function(){
+								$("#altt").show()
+							},function(){
+								$("#altt").hide()
+							}
+						}
                         </script>
                     </div>
                 	                </div>
--- quiz_1/pra01/front/news.php
+++ quiz_1/pra01/front/news.php
@@ -3,10 +3,47 @@
                     	                    </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
-    <div style="text-align:center;">
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-        <a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
-    </div>
+    <h3>最多最新消息顯示區</h3>
+	<hr>
+	<div>
+		<?php
+		$total=${ucfirst($do)}->count();
+		$now=$_GET['p']??1;
+		$div=5;
+		$pages=ceil($total/$div);
+		$start=($now-1)*$div;
+		$rows=${ucfirst($do)}->all(" limit $start,$div");
+		
+		?>
+		<ol>
+			<?php
+			foreach($rows as $row){
+				echo "<li class='ssww'>";
+				echo mb_substr($row['text'],0,25);
+				echo "<span class='all' style='display:none'>{$row['text']}</span>";
+				echo "<li>";
+			?>
+			<?php
+			}
+			?>
+		</ol>
+	</div>
+	<div class="cent">
+		<?php
+		if($now-1>=1){
+			$prev=$now-1;
+			echo "<a href='?do=$do&p=$prev'><</a>";
+		}
+		for($i=1;$i<=$pages;$i++){
+			$font=($i==$now)?"20px":"18px";
+			echo "<a href='?do=$do&p=$i' style='font-size:$font'>$i</a>";
+		}
+		if($now+1<=$pages){
+			$next=$now+1;
+			echo "<a href='?do=$do&p=$next'>></a>";
+		}
+		?>
+	</div>
 </div>
 
 <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
